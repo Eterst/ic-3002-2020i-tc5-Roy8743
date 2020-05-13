4607ad95f5bfc294d6b345a296ca50ede8dc1c06
Roy8743 <57109928+Roy8743@users.noreply.github.com>
2020-05-12 23:06:22 -0600

    Update burbuja.py

--- burbuja.py
+++ burbuja.py
@@ -2,6 +2,46 @@ def burbuja(A):
     n = len(A)
     for i in range(1, n):
         for j in range(0, n - i):
+            #print("paso")
+            if A[j] > A[j + 1]:
+                swap(A, j, j + 1)
+    return A
+
+
+def swap(A, i, j):
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+
+def burbuja_optimizado(A):
+	n = len(A)
+	cambio=False
+	for i in range(1, n):
+		print("paso")
+		for j in range(0, n - i):
+			if A[j] > A[j + 1]:
+				swap(A, j, j + 1)
+				cambio=True
+		if(cambio==False):
+			return A
+		cambio=False
+
+	return A
+
+#p=burbuja_optimizado([6,5,4,3,2,1])
+#print(p)
+
+
+
+
+
+
+
+
+"""def burbuja(A):
+    n = len(A)
+    for i in range(1, n):
+        for j in range(0, n - i):
             if A[j] > A[j + 1]:
                 swap(A, j, j + 1)
 
@@ -12,3 +52,6 @@ def swap(A, i, j):
 
 def burbuja_optimizado(A):
     raise NotImplementedError()
+    
+    
+"""
